6871a29626d4d1de6dcf24632686ad1781089827
Tofu <urmombanger2008@gmail.com>
2025-03-10 11:14:59 +0300

    Create WelcomeScreen.js

---
+++ WelcomeScreen.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function WelcomeScreen() {
+  return (
+    <div
+      style={{
+        width: '100%',
+        height: '100%',
+        background: 'white',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: '2em',
+        color: 'black',
+      }}
+    >
+      Нажмите, чтобы начать
+    </div>
+  );
+}
+
+export default WelcomeScreen;
